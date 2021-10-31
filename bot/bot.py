6e31c1afb714d9fcafe0fc130d8274961bbedaad
koralowiec <arek0906@protonmail.com>
2021-10-31 08:47:54 +0100

    Improvement: ability to set prefix via env.

--- bot/bot.py
+++ bot/bot.py
@@ -3,11 +3,14 @@ from bot.send_info_cog import PushMessageCog
 
 from bot.stats_cog import StatsCog
 
+default_prefix = "!covid"
 
-bot = commands.Bot(command_prefix="!covid ")
 
-
-def run_bot(token: str):
+def run_bot(token: str, prefix: str = default_prefix):
+    # Appending a space after the prefix
+    prefix = f"{prefix} "
+    bot = commands.Bot(command_prefix=prefix)
+    print(f"Bot will be available with prefix: {prefix}")
     print("Bot is starting")
     bot.add_cog(StatsCog(bot))
     bot.add_cog(PushMessageCog(bot))
--- main.py
+++ main.py
@@ -8,12 +8,16 @@ from bot.bot import run_bot
 if __name__ == "__main__":
     load_dotenv()
     TOKEN = os.getenv("TOKEN")
+    PREFIX = os.getenv("PREFIX")
 
     if TOKEN is None:
         print("Provide Discord's Token via TOKEN environment variable!")
         exit()
 
     Database.connect()
+
+    if PREFIX is not None:
+        run_bot(TOKEN, prefix=PREFIX)
 
     run_bot(TOKEN)
 
